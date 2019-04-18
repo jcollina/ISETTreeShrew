fc2f6014e1d932afa5aecf5b44be504928fdf259
Jared <jscollina@gmail.com>
2019-04-17 21:25:59 -0400

    improving tree shrew eye geometry code
    Signed-off-by: Jared <jscollina@gmail.com>

--- compute/prac_eyeGeom.m
+++ compute/prac_eyeGeom.m
@@ -1,7 +1,7 @@
 
 ts_AL = 8; %mm
 
-ts_FL = 6; %mm
+ts_FL = 5; %mm
 
 fineness = 1; %0/1/2: step size of 1.0/0.1/0.01... visual degrees
 
@@ -18,23 +18,30 @@ ts_CD_coarse = [16, 20, 24, 28, 32, 32, 28, 24]; %1000 cones/mm^2
 
 % looking at the figures: where is the center of the pupil? 
 %   
-central_Loc = 8; 
+central_Loc = (max(ts_fixed_loc)-min(ts_fixed_loc))/2; 
+
 
 rel_R = ts_fixed_loc - central_Loc;
-%vis_degrees = atand(rel_R/ts_FL);
-%rel_R = 15;
 %
+
+maxR = max(abs(rel_R));
+
+a = (maxR/(ts_AL/2));
+proj_ecc = (ts_AL/2) * sin(a);
+dist = (ts_AL/2) * cos(a);
+
+theor_FL = proj_ecc/tand(88) - dist + (ts_AL/2);
+
 a = (rel_R ./ (ts_AL/2));
-%
 proj_ecc = (ts_AL/2) .* sin(a);
 dist = (ts_AL/2) .* cos(a);
-focal_dist = dist - (ts_AL/2) + ts_FL;
-
-vis_degrees = atand(proj_ecc ./ focal_dist) + angleShift - returnShift
+focal_dist = dist - (ts_AL/2) + theor_FL;
+vis_degrees = atand(proj_ecc ./ focal_dist) + angleShift - returnShift;
 
 ts_VD_R = min(round(vis_degrees)):(10^(-1*fineness)):max(round(vis_degrees));
-
 ts_CD_R = interp1(vis_degrees,ts_CD_coarse,ts_VD_R,'spline');
+
+%range(vis_degrees)
 
 %%
 
@@ -42,17 +49,13 @@ rel_L = central_Loc - ts_fixed_loc;
 %vis_degrees = atand(rel_L/ts_FL);
 
 a = (rel_L ./ (ts_AL/2));
-%
 proj_ecc = (ts_AL/2) .* sin(a);
 dist = (ts_AL/2) .* cos(a);
-focal_dist = dist - (ts_AL/2) + ts_FL;
+focal_dist = dist - (ts_AL/2) + theor_FL;
 vis_degrees = atand(proj_ecc ./ focal_dist) - angleShift + returnShift;
 
 ts_VD_L = min(round(vis_degrees)):(10^(-1*fineness)):max(round(vis_degrees));
 ts_CD_L = interp1(vis_degrees,ts_CD_coarse,ts_VD_L,'spline');
-
-range(vis_degrees)
-
 
 %%
 m_L = [ts_VD_L',ts_CD_L'];
